008503a531933bfe3b79d57a00a13d1b3b7e642b
MizukiFurusawa <fmzk0326@gmail.com>
2018-06-05 22:36:53 +0900

    Added marker sample

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 # -*- coding: utf-8 -*-
 import pygame
+from gevent import os
 from pygame.locals import *
 import sys
 
 
+def split_image(image):
+    imageList = []
+    for i in range(0, 800, 160):
+        for j in range(0, 800, 160):
+            surface = pygame.Surface((160, 160))
+            surface.blit(image, (0, 0), (j, i, j + 160, i + 160))
+            imageList.append(surface)
+    return imageList
+
+
 def main():
-    pygame.init()  # 初期化
-    screen = pygame.display.set_mode((600, 400))  # ウィンドウサイズの指定
-    pygame.display.set_caption("Pygame Test")  # ウィンドウの上の方に出てくるアレの指定
+    pygame.init()
+    screen = pygame.display.set_mode((640 + 32 * 5, 640 + 32 * 5))
+    pygame.display.set_caption("jbt-simulator")
+    image = split_image(pygame.image.load(os.path.join('img', 'syoku.png')))
+    index = 0
 
     while True:
-        screen.fill((255, 63, 10,))  # 背景色の指定。RGBだと思う
-        pygame.display.update()  # 画面更新
+        screen.fill((32, 32, 32))
 
-        for event in pygame.event.get():  # 終了処理
+        for i in range(32, 640, 160 + 32):
+            for j in range(32, 640, 160 + 32):
+                screen.blit(image[index % 25], (j, i))
+
+        index = index + 1
+        pygame.display.update()
+
+        for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
